8074d890dd84375cc16f28998adb930ff8f68c60
Kristy2594 <k.primakova2017@yandex.com>
2026-04-24 14:52:44 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Ball {
+
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Колобок съеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
+    }
+
+    public static void main(String[] args) {
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        Ball ball = new Ball();
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        fox.tryEat(ball);
+    }
+}
